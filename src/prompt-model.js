9079ae4ccd3c848f933ea1fd3bde78e8d50e3469
Miguel <miguelsribeiro7@gmail.com>
2024-12-12 00:12:50 +0000

    removed deprecated model

--- src/prompt-model.js
+++ src/prompt-model.js
@@ -1,5 +1,5 @@
 const prompt = [
-  `
+	`
 <br>UNLOAD de dados da tabela TAB01_TABELA01 para obter os campos CATEGORIA1, CATEGORIA2, CATEGORIA3, CATEGORIA4, CATEGORIA5 para o ficheiro de output TBLL.S.TBLLD001.TAB01. 
 No acesso à tabela devem ser considerados os seguintes critérios: 
 - Campo TIPO = 'Contrato'
@@ -7,14 +7,14 @@ No acesso à tabela devem ser considerados os seguintes critérios:
 - Campo DATA > '2024-01-01'
 `,
 
-  '<br> SORT do ficheiro TBLL.S.TBLLD002.DADOS para exclusão dos registos com APLICACAO (posição 6) igual a "DC" e "EM" e REGISTO (posição 20) igual a "F", gerando o ficheiro TBLL.S.TBLLD002.SORT01 and request the field positions, instead of using the field name.',
+	'<br> SORT do ficheiro TBLL.S.TBLLD002.DADOS para exclusão dos registos com APLICACAO (posição 6) igual a "DC" e "EM" e REGISTO (posição 20) igual a "F", gerando o ficheiro TBLL.S.TBLLD002.SORT01 and request the field positions, instead of using the field name.',
 
-  '<br>ICETOOL para obtenção de dois ficheiros de output, utilizando como input o ficheiro TBLL.S.TBLLD003.INPUT, obedecendo às seguintes condições:' +
-    '<br>' +
-    '<br>- TBLL.S.TBLLD003.FICH01 - Ficheiro resultante do somatório do campo numérico MONTANTE (15 bytes de tamanho, começando na posição 22), por PARCEIRO (14 bytes de tamanho, começando no byte 1), e por CLIENTE (7 bytes de tamanho, começando no byte 15).<br>' +
-    '<br>- TBLL.S.TBLLD003.FICH02 - Ficheiro resultante do somatório do campo numérico MONTANTE (15 bytes de tamanho, começando na posição 22), por PARCEIRO (14 bytes de tamanho, começando no byte 1), e CONTRATO (8 bytes de tamanho, começando no byte 50).<br>',
+	"<br>ICETOOL para obtenção de dois ficheiros de output, utilizando como input o ficheiro TBLL.S.TBLLD003.INPUT, obedecendo às seguintes condições:" +
+		"<br>" +
+		"<br>- TBLL.S.TBLLD003.FICH01 - Ficheiro resultante do somatório do campo numérico MONTANTE (15 bytes de tamanho, começando na posição 22), por PARCEIRO (14 bytes de tamanho, começando no byte 1), e por CLIENTE (7 bytes de tamanho, começando no byte 15).<br>" +
+		"<br>- TBLL.S.TBLLD003.FICH02 - Ficheiro resultante do somatório do campo numérico MONTANTE (15 bytes de tamanho, começando na posição 22), por PARCEIRO (14 bytes de tamanho, começando no byte 1), e CONTRATO (8 bytes de tamanho, começando no byte 50).<br>",
 
-  `<br>Se ficheiro TBLL.S.TBLLD004.INPUT tiver conteúdo, executa o programa TBR00010. Este programa terá como input os seguintes ficheiros:
+	`<br>Se ficheiro TBLL.S.TBLLD004.INPUT tiver conteúdo, executa o programa TBR00010. Este programa terá como input os seguintes ficheiros:
   
     - INPUT - TBLL.S.TBLLD004.INPUT
     - DATA - TBLL.S.TBLLD004.DATA
@@ -26,11 +26,11 @@ No acesso à tabela devem ser considerados os seguintes critérios:
   - ERRO - TBLL.S.TBLLD004.ERRO
 `,
 
-  '<br>Criar programa COBOL que efetue cálculo de operações matemáticas simples (adição, subtração, multiplicação, divisão) de dois valores, em que o operador e os valores são recebidos por SYSIN, apresentando o resultado da operação via display.',
+	"<br>Criar programa COBOL que efetue cálculo de operações matemáticas simples (adição, subtração, multiplicação, divisão) de dois valores, em que o operador e os valores são recebidos por SYSIN, apresentando o resultado da operação via display.",
 ];
 
 const system = [
-  `<br>You are an expert in JCL programming. Generate high-quality JCL code based on the following requirements: 
+	`<br>You are an expert in JCL programming. Generate high-quality JCL code based on the following requirements: 
   
 - Ensure that every line generated has a maximum lenght of 72.
 - Ensure that in EMPTY instructions the file name does not have quotes or apostrophes.
@@ -55,7 +55,7 @@ Example of comment section:
 //**
 //* [STEP NAME] -                                                      *
 //**`,
-  `<br>You are an expert in JCL programming. Generate high-quality JCL code based on the following requirements: 
+	`<br>You are an expert in JCL programming. Generate high-quality JCL code based on the following requirements: 
   
 - Ensure that every line generated has a maximum lenght of 72.
 - Ensure that in EMPTY instructions the file name does not have quotes or apostrophes.
@@ -67,16 +67,16 @@ Example of comment section:
 <br>
 `,
 
-  '<br>You are a seasoned expert in COBOL programming, specializing in the development of robust batch processing applications. Your task is to write clean, efficient, and well-documented code that adheres to industry best practices. Ensure the code includes comprehensive comments for clarity, effectively handles potential edge cases, and is optimized for performance and maintainability. Please keep explanations and notes to a minimum, focusing on delivering high-quality COBOL code that meets the specified requirements.',
+	"<br>You are a seasoned expert in COBOL programming, specializing in the development of robust batch processing applications. Your task is to write clean, efficient, and well-documented code that adheres to industry best practices. Ensure the code includes comprehensive comments for clarity, effectively handles potential edge cases, and is optimized for performance and maintainability. Please keep explanations and notes to a minimum, focusing on delivering high-quality COBOL code that meets the specified requirements.",
 ];
 
 // Model Change is done here
 const model = [
-  'mixtral-8x7b-32768',
-  'llama3-8b-8192',
-  'llama3-70b-8192',
-  'gemma-7b-it',
-  'gemma2-9b-it',
+	"mixtral-8x7b-32768",
+	"llama3-8b-8192",
+	"llama3-70b-8192",
+	"llama-guard-3-8b",
+	"gemma2-9b-it",
 ];
 
 export { prompt, model, system };
